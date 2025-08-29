23159b4ee2c17ad070064dc5bf0e2a0db58bef1e
Wellington <wellingtongmb@gmail.com>
2025-08-29 19:52:48 -0300

    Chaging tecnology backend for Java with Spring + Gradle

---
+++ coffee-delivery-api/src/main/java/com/wellington/coffee_delivery_api/CoffeeDeliveryApiApplication.java
@@ -0,0 +1,13 @@
+package com.wellington.coffee_delivery_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeDeliveryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeDeliveryApiApplication.class, args);
+	}
+
+}
